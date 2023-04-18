608e194d4f75aefc0e19cc63eee0e567e2e7c82a
peter <secret_wings@naver.com>
2023-04-18 15:15:20 +0900

    fix: SSR 사용 가능 모듈 변경

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,4 @@
 export { default as StepScroll } from "./StepScroll/StepScroll";
+export { default as Page } from "./StepScroll/Page";
 export { useStepScroll } from "./StepScroll/hooks/useStepScroll";
 export * from "./StepScroll/hooks";
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/lib/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  optimization: {
+    minimize: false
+},
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    },
+  },
+};
